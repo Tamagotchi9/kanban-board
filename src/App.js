0e47ce12228eff937ee50de9f61c7822b95ee13a
Tamagotchi9 <voloshko999@gmail.com>
2022-09-06 22:41:46 +0300

    add default board on close setup popup

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Popup } from "./components/Popup";
 import { BoardSettingsForm } from "./components/BoardSettingsForm";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+import { boardSlice } from "./store/modules/board";
 
 function App() {
+  const dispatch = useDispatch();
   const [modal, setModal] = useState(true);
+
+  const onCloseSetupForm = useCallback(() => {
+    dispatch(boardSlice.actions.setBoard([]));
+    setModal(false);
+  }, [setModal, dispatch]);
 
   return (
     <DndProvider backend={HTML5Backend}>
       <Popup
         isOpen={modal}
-        close={() => setModal(false)}
+        close={onCloseSetupForm}
         modalTitle="Setup your board"
       >
         <BoardSettingsForm closeModal={() => setModal(false)} />
